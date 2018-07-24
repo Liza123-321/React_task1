9c8883829c5e9c65a1b63116a8a350845462ccfc
Lizaveta Kaliaha <liza-98k@mail.ru>
2018-07-24 12:38:25 +0300

    added style.js

--- src/views/Counter/index.js
+++ src/views/Counter/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import {withStyles} from "@material-ui/core/styles/index";
+import styles from './style'
 
 
 class Counter extends React.Component{
     render(){
+        const { classes } = this.props;
         return(
             <div>
-                <TextField value={this.props.counterValue}/>
-                <br/>      <br/>
-                <Button variant="raised" color="default" onClick={this.props.incrementFunction}>
+                <TextField className={classes.textField} value={this.props.counterValue}/>
+                <br/><br/>
+                <Button variant="raised" color="default" className={classes.button} onClick={this.props.incrementFunction}>
                     increment
                 </Button>
-                <Button  variant="raised" color="secondary" onClick={this.props.resetFunction}>
+                <Button  variant="raised" color="secondary" className={classes.button} onClick={this.props.resetFunction}>
                     reset
                 </Button>
-                <Button  variant="raised" color="default" onClick={this.props.decrementFunction}>
+                <Button  variant="raised" color="default" className={classes.button} onClick={this.props.decrementFunction}>
                     decrement
                 </Button>
 
@@ -23,4 +26,5 @@ class Counter extends React.Component{
         );
     }
 }
-export default Counter;
+
+export default withStyles(styles)(Counter);
--- src/views/Counter/style.js
+++ src/views/Counter/style.js
@@ -1,3 +1,4 @@
+
 const styles = theme => ({
     button: {
         margin: theme.spacing.unit,
@@ -5,6 +6,18 @@ const styles = theme => ({
     input: {
         display: 'none',
     },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    menu: {
+        width: 200,
+    },
 });
 
- export default styles;
+export default styles;
